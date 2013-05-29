e3e98a3aa48e82c5497099151d2ebdf074ddf128
Aur Saraf <sonoflilit@gmail.com>
2013-05-30 02:36:26 +0300

    pong: static ball

--- pong/main.py
+++ pong/main.py
@@ -1,8 +1,18 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty
 
 class PongGame(Widget):
     pass
+
+class PongBall(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
 
 class PongApp(App):
 
@@ -11,16 +21,3 @@ class PongApp(App):
 
 if __name__ == '__main__':
     PongApp().run()
-# from kivy.app import App
-# from kivy.uix.widget import Widget
-
-# class PongGame(Widget):
-#     pass
-
-# class PongApp(App):
-
-#     def build(self):
-#         return PongGame()
-
-# if __name__ == '__main__':
-#     PongApp().run()
